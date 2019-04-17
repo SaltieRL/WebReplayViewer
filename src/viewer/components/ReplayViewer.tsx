d326e6a5beed7c53f1727276107fc51a5e11949d
Abbondanzo <peter@abbondanzo.com>
2019-04-16 20:55:58 -0400

    Built default files

---
+++ src/viewer/components/ReplayViewer.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from "react"
+
+interface Props {}
+
+class ReplayViewer extends PureComponent<Props> {
+  render() {
+    return <div>Hello</div>
+  }
+}
+
+export default ReplayViewer
