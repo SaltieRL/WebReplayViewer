a6b49f076640f77b65a4858e37dea7916793bc4f
Abbondanzo <peter@abbondanzo.com>
2019-04-17 23:12:53 -0400

    Build basic example React app

---
+++ examples/app/App.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { ReplayViewer } from "../../src"
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h2>Welcome to React</h2>
+      </div>
+      <p>
+        To get started, edit <code>app/App.tsx</code> and save to reload.
+      </p>
+      <ReplayViewer />
+    </div>
+  )
+}
+
+export default App
