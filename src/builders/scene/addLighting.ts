0503071439d9825d2263a78cb36e18299f81ed59
Abbondanzo <peter@abbondanzo.com>
2019-05-15 21:44:22 -0400

    Use white light for the scene

--- src/builders/scene/addLighting.ts
+++ src/builders/scene/addLighting.ts
@@ -2,7 +2,7 @@ import { AmbientLight, HemisphereLight, Scene } from "three"
 
 export const addLighting = (scene: Scene) => {
   // Ambient light
-  scene.add(new AmbientLight(0x444444))
+  scene.add(new AmbientLight(0xffffff))
 
   // Hemisphere light
   scene.add(new HemisphereLight(0xffffbb, 0x080820, 1))
