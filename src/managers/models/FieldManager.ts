87a78b852b7b8be487bec3109e0e315ec330c85b
Abbondanzo <peter@abbondanzo.com>
2019-05-18 04:52:31 -0400

    Add camera getter to field manager

--- src/managers/models/FieldManager.ts
+++ src/managers/models/FieldManager.ts
@@ -1,10 +1,16 @@
-import { Group } from "three"
+import { Group, Camera } from "three"
 
 class FieldManager {
   readonly field: Group
+  readonly cameras: Camera[]
 
-  constructor(field: Group) {
+  constructor(field: Group, cameras: Camera[]) {
     this.field = field
+    this.cameras = cameras
+  }
+
+  getCamera(cameraName: string) {
+    return this.cameras.find(camera => camera.name === cameraName)
   }
 }
 
