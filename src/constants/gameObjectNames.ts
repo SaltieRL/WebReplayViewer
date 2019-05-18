073c3aa3ef8452a5808e16906abf058666df92c2
Abbondanzo <peter@abbondanzo.com>
2019-05-18 04:53:12 -0400

    Add camera object names

--- src/constants/gameObjectNames.ts
+++ src/constants/gameObjectNames.ts
@@ -2,3 +2,8 @@ export const BALL = "ball"
 export const SPRITE = "sprite"
 export const CAR_SUFFIX = "-car"
 export const GROUP_SUFFIX = "-group"
+
+export const BLUE_GOAL_CAMERA = "Blue Goal Camera"
+export const ORANGE_GOAL_CAMERA = "Orange Goal Camera"
+export const ABOVE_FIELD_CAMERA = "Above Field Camera"
+export const ORTHOGRAPHIC_CAMERA = "Orthographic Camera"
