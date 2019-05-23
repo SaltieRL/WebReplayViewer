942b5010030931ce1dfd481788df1c8244d8b20c
Abbondanzo <peter@abbondanzo.com>
2019-05-22 22:46:26 -0400

    Update wheel positioning

--- src/builders/player/positionWheels.ts
+++ src/builders/player/positionWheels.ts
@@ -3,7 +3,7 @@ import { Group, Object3D } from "three"
 export const positionWheels = (wheelObject: Object3D) => {
   const LEFT_DISTANCE = 55
   const FORWARD_DISTANCE = 80
-  const VERTICAL_DISTANCE = 32
+  const VERTICAL_DISTANCE = 31
   const wheelGroup = new Group()
   const frontLeft = wheelObject.clone()
   frontLeft.name = "Front Left"
@@ -11,12 +11,14 @@ export const positionWheels = (wheelObject: Object3D) => {
   const frontRight = wheelObject.clone()
   frontRight.name = "Front Right"
   frontRight.position.set(FORWARD_DISTANCE, -VERTICAL_DISTANCE, LEFT_DISTANCE)
+  frontRight.scale.z = -1
   const backLeft = wheelObject.clone()
   backLeft.name = "Back Left"
   backLeft.position.set(-FORWARD_DISTANCE, -VERTICAL_DISTANCE, -LEFT_DISTANCE)
   const backRight = wheelObject.clone()
   backRight.name = "Back Right"
   backRight.position.set(-FORWARD_DISTANCE, -VERTICAL_DISTANCE, LEFT_DISTANCE)
+  backRight.scale.z = -1
 
   wheelGroup.add(frontLeft, frontRight, backLeft, backRight)
   return wheelGroup
