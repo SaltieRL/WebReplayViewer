cb911f0e7df3cdd74a6917a6d2238c5db1de2795
Abbondanzo <peter@abbondanzo.com>
2019-06-06 23:17:26 -0400

    Hash animation name getters

--- src/builders/utils/playerNameGetters.ts
+++ src/builders/utils/playerNameGetters.ts
@@ -1,9 +1,10 @@
 import { CAR_SUFFIX, GROUP_SUFFIX } from "../../constants/gameObjectNames"
+import { hashCode } from "../../utils/hashCode"
 
 export const getCarName = (playerName: string) => {
-  return `${playerName}${CAR_SUFFIX}`
+  return `${hashCode(playerName)}${CAR_SUFFIX}`
 }
 
 export const getGroupName = (playerName: string) => {
-  return `${playerName}${GROUP_SUFFIX}`
+  return `${hashCode(playerName)}${GROUP_SUFFIX}`
 }
