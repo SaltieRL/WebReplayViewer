67a006724d1b76a0358042119f93b55f29e89340
Abbondanzo <peter@abbondanzo.com>
2019-06-21 00:53:52 -0400

    Subscribe to camera events inside player manager

--- src/managers/models/PlayerManager.ts
+++ src/managers/models/PlayerManager.ts
@@ -1,6 +1,14 @@
 import { Group, PerspectiveCamera, Vector3 } from "three"
 
 import { SPRITE } from "../../constants/gameObjectNames"
+import {
+  addCameraChangeListener,
+  CameraChangeEvent,
+} from "../../eventbus/events/cameraChange"
+import {
+  addCameraFrameUpdateListener,
+  CameraFrameUpdateEvent,
+} from "../../eventbus/events/cameraFrameUpdate"
 
 const CAMERA_ABOVE_PLAYER = 200
 
@@ -10,21 +18,33 @@ export default class PlayerManager {
   readonly isOrangeTeam: boolean
 
   readonly camera: PerspectiveCamera
+  private activeCamera: boolean
 
   constructor(playerName: string, isOrangeTeam: boolean, carGroup: Group) {
     this.playerName = playerName
     this.carGroup = carGroup
     this.isOrangeTeam = isOrangeTeam
     this.camera = new PerspectiveCamera(80, 2, 0.1, 20000)
+    this.activeCamera = false
     this.carGroup.add(this.camera)
+
+    addCameraChangeListener(this.onCameraChange)
+    addCameraFrameUpdateListener(this.onCameraFrameUpdate)
   }
 
-  toggleSprite(display: boolean) {
-    const sprite = this.carGroup.children.find(child => child.name === SPRITE)
-    sprite!.visible = display
+  onCameraChange = ({ camera }: CameraChangeEvent) => {
+    const isActiveCamera = camera === this.camera
+    this.toggleSprite(!isActiveCamera)
+    this.activeCamera = isActiveCamera
   }
 
-  updateCamera({ ballPosition }: UpdateCameraOptions) {
+  onCameraFrameUpdate = ({ ballPosition }: CameraFrameUpdateEvent) => {
+    // Ignore frame updates if we aren't the active camera
+    if (!this.activeCamera) {
+      return
+    }
+
+    // Compute the position where the camera should sit on opposite side of player from ball
     const vectorToBall = new Vector3()
     const scaleFromPlayer = 300
     vectorToBall.subVectors(this.carGroup.position, ballPosition)
@@ -50,10 +70,9 @@ export default class PlayerManager {
     // }
     camera.position.copy(vectorToBall)
   }
-}
 
-interface UpdateCameraOptions {
-  ballPosition: Vector3
-  ballCam: boolean
-  isUsingBoost: boolean
+  private toggleSprite(display: boolean) {
+    const sprite = this.carGroup.children.find(child => child.name === SPRITE)
+    sprite!.visible = display
+  }
 }
