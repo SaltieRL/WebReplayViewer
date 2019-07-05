4118c32c6738f947756c377484fe2b01ebf76b5b
Abbondanzo <peter@abbondanzo.com>
2019-07-05 18:57:00 -0400

    Add free camera to valid cameras

--- src/constants/gameObjectNames.ts
+++ src/constants/gameObjectNames.ts
@@ -6,6 +6,7 @@ export const GROUP_SUFFIX = "-group"
 export const BLUE_GOAL_CAMERA = "Blue Goal Camera"
 export const ORANGE_GOAL_CAMERA = "Orange Goal Camera"
 export const ABOVE_FIELD_CAMERA = "Above Field Camera"
+export const FREE_CAMERA = "Free Camera"
 export const ORTHOGRAPHIC = {
   ABOVE_FIELD: "ORTHOGRAPHIC_ABOVE_FIELD",
   BLUE_LEFT: "ORTHOGRAPHIC_BLUE_LEFT",
