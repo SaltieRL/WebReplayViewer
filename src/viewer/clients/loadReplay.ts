26ee817769aad28c280c6e31d1a7f16817cb0409
Abbondanzo <peter@abbondanzo.com>
2019-10-06 18:23:59 -0400

    Cache replay position requests

--- src/viewer/clients/loadReplay.ts
+++ src/viewer/clients/loadReplay.ts
@@ -10,11 +10,25 @@ const fetchByURL = (url: string) =>
     },
   }).then(response => response.json())
 
-export const loadReplay = (
-  replayId: string
+const cache: { [key: string]: [ReplayData, ReplayMetadata] } = {}
+
+export const loadReplay = async (
+  replayId: string,
+  cached?: boolean
 ): Promise<[ReplayData, ReplayMetadata]> => {
-  return Promise.all([
-    fetchByURL(`https://calculated.gg/api/replay/${replayId}/positions`),
-    fetchByURL(`https://calculated.gg/api/v1/replay/${replayId}?key=1`),
-  ])
+  const fetch = () =>
+    Promise.all([
+      fetchByURL(`https://calculated.gg/api/replay/${replayId}/positions`),
+      fetchByURL(`https://calculated.gg/api/v1/replay/${replayId}?key=1`),
+    ])
+  if (cached) {
+    if (!cache[replayId]) {
+      return fetch().then(data => {
+        cache[replayId] = data
+        return data
+      })
+    }
+    return Promise.resolve(cache[replayId])
+  }
+  return fetch()
 }
