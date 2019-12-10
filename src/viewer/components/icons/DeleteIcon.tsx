9adde4f88779d24b924b758eb482dec0532685f2
ipiv <hello.ipiv@gmail.com>
2019-12-10 10:10:21 +0100

    SVG Icons for DrawingControls

---
+++ src/viewer/components/icons/DeleteIcon.tsx
@@ -0,0 +1,24 @@
+/* tslint:disable */
+
+import React from "react"
+
+type Props = {
+  color?: string,
+}
+
+const DeleteIcon = ({color = "#000000"}: Props) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path fill={color} d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+      </svg>
+    </div>
+  )
+}
+
+export default DeleteIcon
---
+++ src/viewer/components/icons/DropDownIcon.tsx
@@ -0,0 +1,23 @@
+/* tslint:disable */
+
+import React from "react"
+
+const DropDownIcon = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path 
+          d="M7,10L12,15L17,10H7Z"
+          fill="#000000"
+        />
+        </svg>
+    </div>
+  )
+}
+
+export default DropDownIcon
