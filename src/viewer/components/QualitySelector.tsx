1ebf044b0cf46c8416b58359a952699bff4edd5d
Abbondanzo <peter@abbondanzo.com>
2020-02-12 20:29:04 -0500

    Add quality selector component

---
+++ src/viewer/components/QualitySelector.tsx
@@ -0,0 +1,58 @@
+import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import Typography from "@material-ui/core/Typography"
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+
+import QualityManager, { QualityOptions } from "../../managers/QualityManager"
+import QualityIcon from "./icons/QualityIcon"
+
+interface Props {
+  qualityManager: QualityManager
+}
+
+const qualityOptions = [
+  { option: QualityOptions.HIGH, text: "High" },
+  { option: QualityOptions.MEDIUM, text: "Medium" },
+  { option: QualityOptions.LOW, text: "Low" },
+]
+
+const QualitySelector = ({ qualityManager }: Props) => {
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [qualitySetting, setQualitySetting] = useState(
+    qualityManager.getQuality()
+  )
+  useEffect(() => {
+    qualityManager.setQuality(qualitySetting)
+  }, [qualitySetting])
+
+  return (
+    <>
+      <Button onClick={() => setDialogOpen(true)}>
+        <QualityIcon />
+      </Button>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <List>
+          {qualityOptions.map(({ option, text }) => (
+            <StyledListItem
+              key={option}
+              button
+              selected={qualitySetting === option}
+              onClick={() => setQualitySetting(option)}
+            >
+              <Typography>{text}</Typography>
+            </StyledListItem>
+          ))}
+        </List>
+      </Dialog>
+    </>
+  )
+}
+
+const StyledListItem = styled(ListItem)`
+  cursor: pointer;
+`
+
+export default QualitySelector
