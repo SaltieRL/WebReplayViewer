df5c9d3af8d1aca196e33a9f46b9097c23e6a80e
Long Tran <lngtrn94@gmail.com>
2020-03-19 21:43:18 +0100

    do not replace material

--- src/loaders/storage/loadRlLoadout.ts
+++ src/loaders/storage/loadRlLoadout.ts
@@ -6,7 +6,7 @@ import {
   RocketAssetManager,
   Wheel,
 } from "rl-loadout-lib"
-import { Mesh, MeshPhongMaterial, MeshStandardMaterial } from "three"
+import { Mesh } from "three"
 
 import { ExtendedPlayer } from "../../models/ReplayMetadata"
 
@@ -50,21 +50,6 @@ export const loadRlLoadout = async (
       const mesh = object as Mesh
       mesh.receiveShadow = true
       mesh.castShadow = true
-
-      // Phong material is less physically accurate but has noticably better performance
-      const oldMaterial = mesh.material as MeshStandardMaterial
-      const phongMaterial = new MeshPhongMaterial()
-      phongMaterial.name = oldMaterial.name
-      phongMaterial.map = oldMaterial.map
-      phongMaterial.normalMap = oldMaterial.normalMap
-      phongMaterial.color = oldMaterial.color
-      phongMaterial.shininess = (1 - oldMaterial.roughness) * 100
-      phongMaterial.skinning = oldMaterial.skinning
-
-      mesh.material = phongMaterial
-      mesh.material.needsUpdate = true
-
-      oldMaterial.dispose()
     }
   })
 
