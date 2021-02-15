b87803b24ce5da723277b919f298aa78a9b70b4f
Abbondanzo <peter@abbondanzo.com>
2021-02-15 15:21:39 -0500

    Fix fullscreen

---
+++ src/viewer/components/Fullscreen.tsx
@@ -0,0 +1,39 @@
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import React, { PropsWithChildren } from "react"
+import { FullScreen as FS, useFullScreenHandle } from "react-full-screen"
+import styled from "styled-components"
+
+import FullscreenExitIcon from "./icons/FullscreenExitIcon"
+import FullscreenIcon from "./icons/FullscreenIcon"
+
+interface Props extends PropsWithChildren<{}> {}
+
+const Fullscreen = ({ children }: Props) => {
+  const handle = useFullScreenHandle()
+  return (
+    <FullscreenWrapper handle={handle}>
+      <FullscreenToggle>
+        <Button onClick={handle.active ? handle.exit : handle.enter}>
+          <Typography style={{ color: "#fff" }} component="div">
+            {handle.active ? <FullscreenExitIcon /> : <FullscreenIcon />}
+          </Typography>
+        </Button>
+      </FullscreenToggle>
+      {children}
+    </FullscreenWrapper>
+  )
+}
+
+const FullscreenToggle = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`
+const FullscreenWrapper = styled(FS)`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`
+
+export default Fullscreen
--- src/viewer/components/ReplayViewer.tsx
+++ src/viewer/components/ReplayViewer.tsx
@@ -1,14 +1,10 @@
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
 import React, { createRef, PureComponent, RefObject } from "react"
-import FullScreen from "react-full-screen"
 import styled from "styled-components"
 
 import { dispatchCanvasResizeEvent } from "../../eventbus/events/canvasResize"
 import { dispatchPlayPauseEvent } from "../../eventbus/events/playPause"
 import { GameManager } from "../../managers/GameManager"
-import FullscreenExitIcon from "./icons/FullscreenExitIcon"
-import FullscreenIcon from "./icons/FullscreenIcon"
+import Fullscreen from "./Fullscreen"
 import Scoreboard from "./ScoreBoard"
 
 interface Props {
@@ -58,32 +54,14 @@ class ReplayViewer extends PureComponent<Props, State> {
     dispatchCanvasResizeEvent({ width, height })
   }
 
-  toggleFullscreen = (enabled: boolean) => {
-    this.setState({
-      fullScreen: enabled,
-    })
-  }
-
   render() {
-    const { fullScreen } = this.state
-    const onClick = () => this.toggleFullscreen(!this.state.fullScreen)
     return (
       <ViewerContainer>
-        <FullscreenWrapper
-          enabled={fullScreen}
-          onChange={this.toggleFullscreen}
-        >
+        <Fullscreen>
           <div style={{ width: "100%", height: "100%" }} ref={this.mount} />
           <Scoreboard />
-          <FullscreenToggle>
-            <Button onClick={onClick}>
-              <Typography style={{ color: "#fff" }}>
-                {fullScreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
-              </Typography>
-            </Button>
-          </FullscreenToggle>
           {this.props.children}
-        </FullscreenWrapper>
+        </Fullscreen>
       </ViewerContainer>
     )
   }
@@ -97,15 +75,6 @@ const ViewerContainer = styled.div`
     width: 100%;
     height: 100%;
   }
-`
-const FullscreenWrapper = styled(FullScreen)`
-  width: 100%;
-  height: 100%;
-`
-const FullscreenToggle = styled.div`
-  position: absolute;
-  bottom: 0;
-  right: 0;
 `
 
 export default ReplayViewer
